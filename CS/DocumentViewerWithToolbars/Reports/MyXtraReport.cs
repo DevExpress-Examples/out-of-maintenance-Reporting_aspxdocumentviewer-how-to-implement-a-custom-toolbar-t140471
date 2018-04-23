c3aba35d5987708e3a849d5d30ea1ac7de0404c7
DevExpressExampleBot <DevExpressExampleBot@users.noreply.github.com>
2018-04-23 19:21:59 +0300

    initial commit

---
+++ CS/DocumentViewerWithToolbars/Reports/MyXtraReport.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using DevExpress.XtraPrinting;
+
+namespace DocumentViewerWithToolbars.Reports
+{
+    public partial class MyXtraReport : DevExpress.XtraReports.UI.XtraReport
+    {
+        int progress;
+
+        public MyXtraReport()
+        {
+            InitializeComponent();
+        }
+
+        void MyXtraReport_BeforePrint(object sender, System.Drawing.Printing.PrintEventArgs e)
+        {
+            ReportPrintOptions.DetailCountOnEmptyDataSource = Math.Max(Math.Min((int)pageCount.Value, 100), 1);
+            PrintingSystem.ProgressReflector.PositionChanged += ProgressReflector_PositionChanged;
+        }
+
+        void MyXtraReport_AfterPrint(object sender, EventArgs e)
+        {
+            PrintingSystem.ProgressReflector.PositionChanged -= ProgressReflector_PositionChanged;
+        }
+
+        void ProgressReflector_PositionChanged(object sender, EventArgs e)
+        {
+            var progressReflector = (ProgressReflector)sender;
+            progress = progressReflector.Position;
+        }
+
+        void xrShape1_BeforePrint(object sender, System.Drawing.Printing.PrintEventArgs e)
+        {
+            xrShape1.Angle = progress * (180 / 100);
+        }
+    }
+}
---
+++ Readme.md
@@ -0,0 +1,8 @@
+# ASPxDocumentViewer - How to implement a custom toolbar
+
+
+This example demonstrates an outdated approach to implementing a custom toolbar.<br><br> Refer to the <a href="https://www.devexpress.com/Support/Center/Example/Details/T144065">How to link the Document Viewer with an external Ribbon control</a> example to see how to create an external Ribbon toolbar and assign it to the Document Viewer. If you need to customize the standard Document Viewer toolbar, use the approach described in the <a href="https://documentation.devexpress.com/#XtraReports/CustomDocument115660">How to: Customize the Document Viewer Toolbar</a> document.
+
+<br/>
+
+
