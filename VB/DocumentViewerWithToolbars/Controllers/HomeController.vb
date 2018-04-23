1dd5cbb6f04a77556c2443a594f8db51ad355cd1
DevExpressExampleBot <DevExpressExampleBot@users.noreply.github.com>
2018-04-23 19:21:59 +0300

    initial commit

---
+++ VB/DocumentViewerWithToolbars/Controllers/HomeController.vb
@@ -0,0 +1,24 @@
+﻿Imports DevExpress.Web.Mvc
+Imports DevExpress.XtraReports.UI
+Imports DocumentViewerWithToolbars.Reports
+Imports System.Web.Mvc
+
+Namespace DocumentViewerWithToolbars.Controllers
+    Public Class HomeController
+        Inherits Controller
+
+        Private ReadOnly report As XtraReport = New MyXtraReport()
+
+        Public Function Index() As ActionResult
+            Return View(report)
+        End Function
+
+        Public Function DocumentViewerPartial() As ActionResult
+            Return PartialView("_DocumentViewerPartial", report)
+        End Function
+
+        Public Function DocumentViewerPartialExport() As ActionResult
+            Return DocumentViewerExtension.ExportTo(report, Request)
+        End Function
+    End Class
+End Namespace
